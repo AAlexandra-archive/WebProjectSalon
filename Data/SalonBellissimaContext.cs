af78ebae61ddfbea6b63f6a2047f24dee5d0db80
AAlexandra-archive <alexalupoae@gmail.com>
2026-05-02 22:40:20 +0300

    Add files via upload

---
+++ Data/SalonBellissimaContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalonBellissima.Models;
+
+namespace SalonBellissima.Data
+{
+    public class SalonBellissimaContext : DbContext
+    {
+        public SalonBellissimaContext (DbContextOptions<SalonBellissimaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalonBellissima.Models.Serviciu> Serviciu { get; set; } = default!;
+        public DbSet<SalonBellissima.Models.Angajat> Angajat { get; set; } = default!;
+        public DbSet<SalonBellissima.Models.Categorie> Categorie { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Serviciu>()
+                .HasOne(s => s.Categorie)
+                .WithMany(c => c.Servicii)
+                .HasForeignKey(s => s.CategorieID);
+        }
+        public DbSet<SalonBellissima.Models.Client> Client { get; set; } = default!;
+        public DbSet<SalonBellissima.Models.Programare> Programare { get; set; } = default!;
+    }
+}
---
+++ Models/Angajat.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalonBellissima.Models
+{
+    public class Angajat
+    {
+        public int ID { get; set; }
+        public string Prenume { get; set; }
+        public string Nume { get; set; }
+
+        [Display(Name = "Nume")]
+        public string NumeAngajat
+        {
+            get { return Prenume + " " + Nume; }
+        }
+        [Display(Name = "Ocupație")]
+        public string Ocupatie { get; set; }
+        public ICollection<AngajatAsociat>? AngajatiAsociati { get; set; }
+    }
+}
